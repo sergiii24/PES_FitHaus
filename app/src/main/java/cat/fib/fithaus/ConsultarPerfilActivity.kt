0f28d8b9fb728c22e3cdff4c6dbbd705cf90b405
OriolPr <oriolprat2000@gmail.com>
2021-03-21 23:02:00 +0100

    Primera i segona pantalla

---
+++ app/src/main/java/cat/fib/fithaus/ConsultarPerfilActivity.kt
@@ -0,0 +1,11 @@
+package cat.fib.fithaus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultarPerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultar_perfil)
+    }
+}
