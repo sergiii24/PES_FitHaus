d3125c70398a32976760506c0e33ad76cc919d1c
albert.minana <albert.minana@estudiantat.upc.edu>
2021-03-25 15:17:54 +0100

    Afegeix la documentació del codi de l'Activity AuthenticationProviders
    S'han documentat la classe i les funcions de l'Activity AuthenticationProviders i s'han inclós alguns comentaris per entendre millor el codi

--- app/src/main/java/cat/fib/fithaus/AuthenticationProviders.kt
+++ app/src/main/java/cat/fib/fithaus/AuthenticationProviders.kt
@@ -18,37 +18,68 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import kotlinx.android.synthetic.main.activity_authentication_providers.*
 
+/** Activity AuthenticationProviders
+ *
+ *  Activitat encarregada d'autenticar l'usuari amb els proveïdors d'accés Google i Facebook
+ *
+ *  @constructor Crea l'Activity AuthenticationProviders
+ *  @author Albert Miñana Montecino
+ */
 class AuthenticationProviders : AppCompatActivity() {
 
-    private val GOOGLE_SIGN_IN = 100
-    private val callbackManager = CallbackManager.Factory.create()
+    private val GOOGLE_SIGN_IN = 100                                // Constante de Google
+    private val callbackManager = CallbackManager.Factory.create()  // Constante de Facebook
 
+    /** Function onCreate
+     *
+     *  Funció encarregada d'establir el tema, mostrar i configurar el contingut de l'activitat
+     *
+     *  @param savedInstanceState
+     *  @author Albert Miñana Montecino
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_FitHaus)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication_providers)
-        setup()
+        setupGoogleFacebookButtons()
     }
 
-    private fun setup(){
+    /** Function setupGoogleFacebookButtons
+     *
+     *  Funció encarregada de configurar els botons d'accés amb Google i Facebook
+     *
+     *  @author Albert Miñana Montecino
+     */
+    private fun setupGoogleFacebookButtons(){
         googleButton.setOnClickListener() {
-            // Configuración
+            // Configuració d'accés amb Google
             val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
 
+            // Creació del client d'autenticació amb Google
             val googleClient = GoogleSignIn.getClient(this, googleConf)
+
+            // Elimina el compte de Google autenticat en aquest moment (per si es disposa de varis comptes de Google al mateix dispositiu)
             googleClient.signOut()
 
+            // Inicia l'activitat d'accés amb Google
             startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
         }
 
         facebookButton.setOnClickListener(){
+            // Elimina el compte de Facebook autenticat en aquest moment
+            LoginManager.getInstance().logOut()
+
+            // Inicia l'activitat d'accés amb Facebook
             LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
+
+            // Operacions de resposta un cop finalizat l'accés amb Facebook
             LoginManager.getInstance().registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
                 override fun onSuccess(result: LoginResult?) {
                     result?.let {
+                        // Mostra l'accés amb Google a la consola de Firebase
                         val token = it.accessToken
                         val credential = FacebookAuthProvider.getCredential(token.token)
                         FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(){
@@ -62,9 +93,7 @@ class AuthenticationProviders : AppCompatActivity() {
                     }
                 }
 
-                override fun onCancel() {
-
-                }
+                override fun onCancel() {}
 
                 override fun onError(error: FacebookException?) {
                     showAlert()
@@ -73,16 +102,28 @@ class AuthenticationProviders : AppCompatActivity() {
         }
     }
 
+    /** Function onActivityResult
+     *
+     *  Funció encarregada de tractar les activitats d'accés amb Google i Facebook
+     *
+     *  @param requestCode
+     *  @param resultCode
+     *  @param data
+     *  @author Albert Miñana Montecino
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // Cas de tractar l'activitat d'accés amb Facebook
         callbackManager.onActivityResult(requestCode, resultCode, data)
 
+        // Cas de tractar l'activitat d'accés amb Google
         if (requestCode == GOOGLE_SIGN_IN){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
                 if (account != null) {
+                    // Mostra l'accés amb Google a la consola de Firebase
                     val credential = GoogleAuthProvider.getCredential(account.idToken, null)
                     FirebaseAuth.getInstance().signInWithCredential(credential)
                         .addOnCompleteListener {
@@ -101,6 +142,13 @@ class AuthenticationProviders : AppCompatActivity() {
         }
     }
 
+    /** Function showHome
+     *
+     *  Funció encarregada de canviar a una altra activitat
+     *
+     *  @param email
+     *  @author Albert Miñana Montecino
+     */
     private fun showHome(email: String){
         val homeIntent = Intent(this, MainActivity::class.java).apply {
             putExtra("email", email)
@@ -108,6 +156,12 @@ class AuthenticationProviders : AppCompatActivity() {
         startActivity(homeIntent)
     }
 
+    /** Function showAlert
+     *
+     *  Funció encarregada de mostrar un missatge d'error d'autenticació de l'usuari
+     *
+     *  @author Albert Miñana Montecino
+     */
     private fun showAlert(){
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
