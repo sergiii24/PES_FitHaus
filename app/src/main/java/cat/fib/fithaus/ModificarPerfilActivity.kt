7c04eea064ebfd754a2a475a307f74231d26bb4c
daniel.cardenas.rafael <daniel.cardenas.rafael@estudiantat.upc.edu>
2021-04-23 12:08:01 +0200

    Documenta totes les classes i funcions
    S'han documentat totes les classes i funcions.

--- app/src/main/java/cat/fib/fithaus/ModificarPerfilActivity.kt
+++ app/src/main/java/cat/fib/fithaus/ModificarPerfilActivity.kt
@@ -3,6 +3,7 @@ package cat.fib.fithaus
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cat.fib.fithaus.api.ApiServices
 import cat.fib.fithaus.models.User
@@ -14,16 +15,29 @@ import cat.fib.fithaus.ui.FragmentModificarFisiques
 import cat.fib.fithaus.ui.FragmentPersonal
 import java.io.IOException
 import com.google.android.gms.security.ProviderInstaller
+import kotlinx.android.synthetic.main.fragment_fisiques.*
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
 
-
+/** Classe Modificar perfil
+ *
+ *  Classe on es mostra la pantalla de modificar perfil.
+ *
+ *  @constructor Indica a l'usuari que es troba a la pantalla de modificar perfil.
+ *  @author Oriol Prat.
+ */
 class ModificarPerfilActivity : AppCompatActivity() {
 
     var userData: User = User()
 
-
+    /** Funció inicialitzadora
+     *
+     *  Funció que fa que es mostri la pantalla a l'usuari i que inicialitza totes les variables.
+     *
+     *  @param savedInstanceState
+     *  @author Daniel Cárdenas.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modificar_perfil)
@@ -39,6 +53,18 @@ class ModificarPerfilActivity : AppCompatActivity() {
         val btnFragmentModificarEsportives: Button = findViewById(R.id.DadesEsportivesModificar)
         val btnFragmentModificarFisiques: Button = findViewById(R.id.DadesFísiquesModificar)
         val btnGuardarModificacions: Button = findViewById(R.id.GuardarModificacions)
+
+        /** Funció campsbuitsfisiques
+         *
+         *  Funció que comprova que els camps a omplir no siguin buits.
+         *
+         *  @return Boolean
+         *  @author Daniel Cárdenas.
+         */
+        fun campsbuitsfisiques(): Boolean {
+            if (fragmentModificarFisiques.Pes.text.isEmpty() || fragmentModificarFisiques.Alçada.text.isEmpty()) return true
+            return false
+        }
 
 
         //Es pot afegir el fragment a una pila per poder tornar-hi quan es tiri endarrere -->  addToBackStack(null)  <-- després de replace
@@ -69,6 +95,7 @@ class ModificarPerfilActivity : AppCompatActivity() {
 
         //Quan cliquem al botó de guardar anem a la pantalla de consultar les dades del perfil d'un usuari
         btnGuardarModificacions.setOnClickListener {
+            if (campsbuitsfisiques()) Toast.makeText(this, "Els camps pes i/o alçada s'han d'omplir", Toast.LENGTH_LONG).show()
             val intent = Intent(this, ConsultarPerfilActivity::class.java)
             startActivity(intent)
         }
@@ -80,6 +107,13 @@ class ModificarPerfilActivity : AppCompatActivity() {
         }*/
     }
 
+
+    /** Funció getUserData
+     *
+     *  Funció que agafa totes les dades d'un usuari.
+     *
+     *  @author Oriol Prat.
+     */
     fun getUserData() {
         //TODO: get id user
         ApiServices.getUserInfo(1, object : Callback {
