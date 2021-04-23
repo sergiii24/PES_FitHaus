26cfd1366b56019612340865475373389d9297ae
adriaespinola <adria.espinola@estudiantat.upc.edu>
2021-04-23 12:17:51 +0200

    Proporciona la documentació de les funcions de l'activitat Qüestionari Inicial
    S'ha afegit la documentació de les diferents funcions d'aquesta activitat.

--- app/src/main/java/cat/fib/fithaus/QuestionariInicialActivity.kt
+++ app/src/main/java/cat/fib/fithaus/QuestionariInicialActivity.kt
@@ -1,5 +1,6 @@
 package cat.fib.fithaus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,48 +14,71 @@ import org.json.JSONObject
 import java.io.IOException
 import cat.fib.fithaus.questionari.QuestionariViewModel
 
+/** Classe Activty Qüestionari Inicial
+ *
+ *  Activity on es troba tota la lògica per poder fer el qüestionari inicial de l'usuari després de registrar-se.
+ *
+ *  @constructor Crea el qüestionari de l'usuari amb tots els camps sense seleccionar.
+ *  @author Adrià Espinola.
+ */
 class QuestionariInicialActivity : AppCompatActivity() {
 
-    private lateinit var questionariViewModel: QuestionariViewModel
+    //private lateinit var questionariViewModel: QuestionariViewModel
 
+    /** Funció inicialitzadora
+     *
+     *  Funció que fa que es mostri la pantalla del qüestionari inicial.
+     *
+     *  @param savedInstanceState
+     *  @author Adrià Espinola.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionari_inicial)
 
-        val appContainer = (application as Application).appContainer
-        questionariViewModel = appContainer.questionariViewModelFactory.create()
+        //val appContainer = (application as Application).appContainer
+        //questionariViewModel = appContainer.questionariViewModelFactory.create()
 
         setupSendButton()
     }
 
+    /** Funció setupSendButton
+     *
+     *  Funció encarregada de configurar el botó d'enviar el qüestionari.
+     *
+     *  @author Adrià Espinola.
+     */
     private fun setupSendButton() {
         buttonEnviar.setOnClickListener {
             send()
         }
     }
 
+    /** Funció send
+     *
+     *  Funció que comprova que el qüestionari sigui omplert correctament.
+     *
+     *  @author Adrià Espinola.
+     */
     private fun send() {
         val experiencia: String? = experience()
-        val categories: MutableList<String> = chosencategories()
+        val categories: MutableList<String> = chosenCategories()
         val countC = categories.size
-        val objectius: MutableList<String> = chosenobjectives()
-        val obj: String = objectius.toString()
+        val objectius: MutableList<String> = chosenObjectives()
         val countO = objectius.size
 
         if ((experiencia == null) || (countC == 0 || countC > 3) || (countO == 0 || countO > 3)) {
             if (experiencia == null)  Toast.makeText(this, "No has seleccionat experiència", Toast.LENGTH_LONG).show()
-            //else Toast.makeText(this, experiencia, Toast.LENGTH_LONG).show()
             if (countC == 0 || countC > 3) Toast.makeText(this, "Selecciona entre 1 i 3 categories", Toast.LENGTH_LONG).show()
             if (countO == 0 || countO > 3) Toast.makeText(this, "Selecciona entre 1 i 3 objectius", Toast.LENGTH_LONG).show()
         }
         else {
-            Toast.makeText(this, countO.toString(), Toast.LENGTH_LONG).show()
-            Toast.makeText(this, experiencia, Toast.LENGTH_LONG).show()
-            println(categories)
-            println(obj)
+            Toast.makeText(this, "Enviat correctament", Toast.LENGTH_LONG).show()
 
-            //questionariViewModel.patchQuestionari("1", experiencia, categories, objectius)
-            // Create JSON using JSONObject
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+
+            // Create JSON using JSONObject for back?
             /*
             val jsonObject = JSONObject()
             jsonObject.put("id", "1")
@@ -68,6 +92,12 @@ class QuestionariInicialActivity : AppCompatActivity() {
         }
     }
 
+    /** Funció experience
+     *
+     *  Funció que retorna el contingut del radio Button seleccionat.
+     *
+     *  @author Adrià Espinola.
+     */
     private fun experience(): String? {
         return when {
             radioButtonPrincipiant?.isChecked == true -> "Princpiant"
@@ -77,7 +107,13 @@ class QuestionariInicialActivity : AppCompatActivity() {
         }
     }
 
-    private fun chosenobjectives(): MutableList<String> {
+    /** Funció chosenObjectives
+     *
+     *  Funció que retorna una llista amb els objectius seleccionats.
+     *
+     *  @author Adrià Espinola.
+     */
+    private fun chosenObjectives(): MutableList<String> {
         val objectius: MutableList<String> = ArrayList()
         if (checkBoxSalut.isChecked) objectius.add("Salut")
         if (checkBoxForça2.isChecked) objectius.add("Força")
@@ -89,7 +125,13 @@ class QuestionariInicialActivity : AppCompatActivity() {
         return objectius
     }
 
-    private fun chosencategories(): MutableList<String> {
+    /** Funció chosenCategories
+     *
+     *  Funció que retorna una llista amb les categories seleccionades.
+     *
+     *  @author Adrià Espinola.
+     */
+    private fun chosenCategories(): MutableList<String> {
         val categories: MutableList<String> = ArrayList()
         if (checkBoxForça.isChecked) categories.add("Força")
         if (checkBoxCardio.isChecked) categories.add("Cardio")
@@ -99,5 +141,4 @@ class QuestionariInicialActivity : AppCompatActivity() {
         if (checkBoxPilates.isChecked) categories.add("Pilates")
         return categories
     }
-
 }
