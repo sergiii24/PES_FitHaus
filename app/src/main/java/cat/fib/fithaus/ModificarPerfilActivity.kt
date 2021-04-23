5d507fb2333442607a87ea467897a0ea8763ed30
daniel.cardenas.rafael <daniel.cardenas.rafael@estudiantat.upc.edu>
2021-04-23 11:02:24 +0200

    Proporciona la comprovació dels camps buits
    Permet comprovar si els camps a actualitzar són buits o no

--- app/src/main/java/cat/fib/fithaus/ModificarPerfilActivity.kt
+++ app/src/main/java/cat/fib/fithaus/ModificarPerfilActivity.kt
@@ -3,6 +3,7 @@ package cat.fib.fithaus
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cat.fib.fithaus.api.ApiServices
 import cat.fib.fithaus.models.User
@@ -13,6 +14,11 @@ import cat.fib.fithaus.ui.FragmentModificarPersonal
 import cat.fib.fithaus.ui.FragmentPersonal
 import java.io.IOException
 import com.google.android.gms.security.ProviderInstaller
+import kotlinx.android.synthetic.main.activity_crear_perfil.*
+import kotlinx.android.synthetic.main.fragment_modificar_personal.*
+import kotlinx.android.synthetic.main.fragment_modificar_personal.Sexe_Altre
+import kotlinx.android.synthetic.main.fragment_modificar_personal.Sexe_Dona
+import kotlinx.android.synthetic.main.fragment_modificar_personal.Sexe_Home
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
@@ -39,6 +45,30 @@ class ModificarPerfilActivity : AppCompatActivity() {
         val btnFragmentModificarFisiques: Button = findViewById(R.id.DadesFísiquesModificar)
         val btnGuardarModificacions: Button = findViewById(R.id.GuardarModificacions)
 
+
+        fun experience(): String? {
+            return when {
+                fragmentModificarPersonal.Sexe_Home?.isChecked == true -> "Home"
+                fragmentModificarPersonal.Sexe_Dona?.isChecked == true -> "Dona"
+                fragmentModificarPersonal.Sexe_Altre?.isChecked == true -> "Altre"
+                else -> null
+            }
+        }
+
+        fun campsbuitspersonals(): Boolean {
+            val sexe: String? = experience()
+            if (fragmentModificarPersonal.name.text.isEmpty() || fragmentModificarPersonal.username.text.isEmpty() || sexe == null) {
+                return true
+            }
+            return false
+        }
+
+        fun campsbuitsesportives(): Boolean {
+            if (fragmentModificarEsportives.nivell.text.isEmpty() || fragmentModificarEsportives.objectiu.text.isEmpty() || fragmentModificarEsportives.categoriainteres.text.isEmpty()) {
+                return true
+            }
+            return false
+        }
 
         //Es pot afegir el fragment a una pila per poder tornar-hi quan es tiri endarrere -->  addToBackStack(null)  <-- després de replace
 
@@ -67,8 +97,12 @@ class ModificarPerfilActivity : AppCompatActivity() {
 
         //Quan cliquem al botó de guardar anem a la pantalla de consultar les dades del perfil d'un usuari
         btnGuardarModificacions.setOnClickListener {
-            val intent = Intent(this, ConsultarPerfilActivity::class.java)
-            startActivity(intent)
+            if (campsbuitspersonals()) Toast.makeText(this, "Els camps nom, usuari i/o sexe s'han d'omplir", Toast.LENGTH_LONG).show()
+            else if (campsbuitsesportives()) Toast.makeText(this, "Els camps nivell, objectiu i/o categories d'interès s'han d'omplir", Toast.LENGTH_LONG).show()
+            else {
+                val intent = Intent(this, ConsultarPerfilActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         //Posem el fragment de dades personals a la pantalla, necessitem el commit per fer efectiu el canvi
@@ -76,7 +110,12 @@ class ModificarPerfilActivity : AppCompatActivity() {
              replace(R.id.flFragment, fragmentModificarPersonal)
              commit()
          }
+
+
     }
+
+
+
 
     fun getUserData() {
         //TODO: get id user
