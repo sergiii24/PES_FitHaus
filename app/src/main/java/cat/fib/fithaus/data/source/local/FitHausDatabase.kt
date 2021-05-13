7e1b446d737c9d3659444f4c04b68cea2a582105
daniel.cardenas.rafael <daniel.cardenas.rafael@estudiantat.upc.edu>
2021-05-13 12:05:32 +0200

    Soluciona problemes de compatibilitat amb els atributs del back-end de tipus array.
    S'ha canviat el model User i les interfícies involucrades per poder rebre i enviar dades correctament al back-end.

--- app/src/main/java/cat/fib/fithaus/data/source/local/FitHausDatabase.kt
+++ app/src/main/java/cat/fib/fithaus/data/source/local/FitHausDatabase.kt
@@ -2,6 +2,9 @@ package cat.fib.fithaus.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import cat.fib.fithaus.data.models.Converters
 import cat.fib.fithaus.data.models.User
 
 /**
@@ -10,6 +13,7 @@ import cat.fib.fithaus.data.models.User
  * Note that exportSchema should be true in production databases.
  */
 @Database(entities = [User::class], version = 1, exportSchema = true)
+@TypeConverters(Converters::class)
 abstract class FitHausDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
