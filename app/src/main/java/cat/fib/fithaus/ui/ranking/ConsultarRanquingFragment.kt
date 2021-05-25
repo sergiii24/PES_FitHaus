459949d9c62f1d35c4ce677738f201777c443afb
daniel.cardenas.rafael <daniel.cardenas.rafael@estudiantat.upc.edu>
2021-05-25 08:29:41 +0200

    Permet accedir a la secció de ranking d'usuaris
    Es pot accedir al ranking d'usuaris des de la barra lateral de l'aplicació.

---
+++ app/src/main/java/cat/fib/fithaus/ui/ranking/ConsultarRanquingFragment.kt
@@ -0,0 +1,31 @@
+package cat.fib.fithaus.ui.ranking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import cat.fib.fithaus.R
+
+class ConsultarRanquingFragment : Fragment() {
+
+    private lateinit var rankingViewModel: RanquingViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        rankingViewModel =
+                ViewModelProvider(this).get(RanquingViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_consultar_ranquing, container, false)
+        val textView: TextView = root.findViewById(R.id.tituloRanking) //es el que estaba de fondo
+        /*rankingViewModel.text.observe(viewLifecycleOwner, Observer {
+            //aquí habrá que hacer la llamada get al ranking.
+        })*/
+        return root
+    }
+}
--- app/src/main/java/cat/fib/fithaus/ui/ranking/RanquingViewModel.kt
+++ app/src/main/java/cat/fib/fithaus/ui/ranking/RanquingViewModel.kt
@@ -1,13 +1,13 @@
-package cat.fib.fithaus.ui.slideshow
+package cat.fib.fithaus.ui.ranking
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class RanquingViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is ranking Fragment"
     }
     val text: LiveData<String> = _text
 }
